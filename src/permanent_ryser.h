f13a2deeb6a7d9c4108f8cb486596bcfa0e78864
Mario Valdivia <96960683+mario-valdivia@users.noreply.github.com>
2022-04-12 15:48:45 +0200

    precommit & fix git dependency on pip install (#3)
    * added pre-commit
    * fix git dependency on pip install

--- src/permanent_ryser.h
+++ src/permanent_ryser.h
@@ -1,26 +1,25 @@
-/*
-MIT License
+// MIT License
+//
+// Copyright (c) 2022 Quandela
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
-Copyright (c) 2022 Quandela
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
 #ifndef _PERMANENT_RYSER_HPP
 #define _PERMANENT_RYSER_HPP
 
@@ -94,7 +93,7 @@ T permanent_ryser_block(const T *A, uint64_t from, uint64_t to, int n) // expect
     for (uint64_t k = from; k < to; k++) {
         uint64_t new_graycode = (k ^ (k >> 1));
         int diff;
-        int size_set = dec2idxarr(chi, diff, new_graycode, prev_size_set); // idx vector        
+        int size_set = dec2idxarr(chi, diff, new_graycode, prev_size_set); // idx vector
         prev_size_set = size_set;
 
         // if we have just one diff with previous line, we can just compute the difference with previous line
